69ef1b359d997987b35e89bf0cde3fc9835c6cd5
Ysmiraak <ysmiraak@gmail.com>
2016-11-11 00:11:37 +0100

    update

--- protocoll/sidebar-items.js
+++ protocoll/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"trait":[["Coll","basic protocol for collections."],["Map","basic protocol for maps."],["Seq","basic protocol for seqs."],["Set","basic protocol for sets."]]});
+initSidebarItems({"trait":[["Map","basic protocol for maps."],["Seq","basic protocol for seqs."],["Set","basic protocol for sets."]]});
